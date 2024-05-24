344bdbdb0c2e5c1e095d9eabe75796cdd854debb
certifieddeveloper085 <certifieddeveloper085@gmail.com>
2024-05-24 10:46:55 -0300

    curriculum

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -7,12 +7,21 @@ import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import NavDropdown from "react-bootstrap/NavDropdown"
 import styles from "../styles/layout.module.css";
+import Image from "next/image";
 
 export const Navc = () => {
   const pathname = usePathname();
 
   return (
     <Navbar expand="lg" className={styles.nav}>
+      <Image
+                src="/code-brackets.svg"
+                className={styles.logo}
+                alt="logo"
+                width={100}
+                height={100}
+                style={{ maxWidth: '3em', maxHeight: '3em' }}
+              />
       <Container>
         <Navbar.Toggle className= "ms-auto" aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse className="ms-auto" id="basic-navbar-nav">
