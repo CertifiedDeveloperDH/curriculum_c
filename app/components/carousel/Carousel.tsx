6be96c16efe32dd05022b97caa56406496286820
certifieddeveloper085 <certifieddeveloper085@gmail.com>
2024-05-30 12:02:53 -0300

    curriculum

--- app/components/carousel/Carousel.tsx
+++ app/components/carousel/Carousel.tsx
@@ -1,38 +1,45 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from "next/image";
+import styles from './Carouselc.module.css'
 
 export const Carouselc = () => {
   return (
     <Carousel>
       <Carousel.Item>
+        <div className={styles.imageContainer}>
         <Image 
         src= "/Java-Logo.png" 
         alt='java_image'
         width={100}
         height={100}
-        style={{ maxWidth: '100em', maxHeight: '100em' }}/>
+        className={styles.image}/>
+        </div>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+      <div className={styles.imageContainer}>
         <Image src='/javascript.png' 
         alt='javascript_image' 
         width={100}
         height={100}
-        style={{ maxWidth: '100em', maxHeight: '100em' }}/>
+        className={styles.image}/>
+        </div>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
+      <div className={styles.imageContainer}>
         <Image src='/CSS-Logo.jpg' 
         alt='css_image'
         width={100}
         height={100}
-        style={{ maxWidth: '100em', maxHeight: '100em' }} />
+        className={styles.image} />
+        </div>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
